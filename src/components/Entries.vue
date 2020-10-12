d6ab07045e6edffc415c0d9b185394228ba469a2
Youri Seichter <youri-S@hotmail.de>
2020-10-12 13:52:48 +0200

    initial commit for exercise_1

---
+++ src/components/Entries.vue
@@ -0,0 +1,47 @@
+<template>
+  <div>
+    <h1>{{ title }}</h1>
+    <div v-bind:key="entry.index" v-for="entry in entries">
+      <Entry
+        v-bind:entry="entry"
+        v-on:del-entry="$emit('del-entry', entry)"
+        @downvote-entry="$emit('downvote-entry', entry)"
+        @upvote-entry="$emit('upvote-entry', entry)"
+      />
+    </div>
+    <div><p></p></div>
+    <form @submit.prevent="addEntry($event, newEntryTitle)">
+      <input type="text" v-model="newEntryTitle" />
+      <input type="submit" value="Add" />
+    </form>
+  </div>
+</template>
+
+<script>
+import Entry from "./Entry";
+
+export default {
+  name: "Entries",
+  components: {
+    Entry,
+  },
+  data() {
+    return {
+      title: "Hackernews",
+      newEntryTitle: "",
+    };
+  },
+  methods: {
+    addEntry(event, title) {
+
+      this.newEntryTitle = "";
+      this.$emit("add-entry", title);
+    },
+  },
+  props: ["entries"],
+};
+</script>
+
+
+<style scoped>
+</style>
